ce8addc8f50b4f6299eb551498f1a490de0b5245
Fred <huertaserick@yahoo.ie>
2024-11-06 00:33:30 +0000

    Initial commits

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,16 @@
+import Barista from "../Barista/Barista";
+
+function Main() {
+  return (
+    <main>
+      <Hero />
+      <About />
+      <Barista />
+      <Menu />
+      <Reviews />
+      <Contact />
+    </main>
+  );
+}
+
+export default Main;
