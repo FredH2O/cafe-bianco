4d19ec345d77248e57f4ce27dd0413111ad9f5ad
Fred <huertaserick@yahoo.ie>
2024-11-07 01:07:59 +0000

    Header and Nav done for now

--- src/index.js
+++ src/index.js
@@ -1,19 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-
-// Import Swiper CSS (correct path)
 import "swiper/css";
-
-// Import Bootstrap CSS (no need for Bootstrap JS if you aren't using jQuery-dependent components)
 import "bootstrap/dist/css/bootstrap.min.css";
-
-// Import jQuery and Bootstrap JS if needed
 import "bootstrap"; // Bootstrap JS
-
-// Import Sticky.js and other custom JS
 import "sticky-js"; // Assuming Sticky.js is used in custom JS
-import "./assets/custom.js"; // Custom JS file that uses jQuery and Sticky.js
 import "./assets/tooplate-barista.css"; // Template-specific CSS
 
 import App from "./App";
