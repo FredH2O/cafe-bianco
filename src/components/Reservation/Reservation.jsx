ae9e5e1714eefeb4bbd61e6bc465a83980eb9d1c
Fred <huertaserick@yahoo.ie>
2024-11-19 20:52:38 +0000

    Form submission

--- src/components/Reservation/Reservation.jsx
+++ src/components/Reservation/Reservation.jsx
@@ -16,6 +16,8 @@ export default function Reservation() {
     specialRequests: "",
   });
 
+  const [submitted, setSubmitted] = useState(false);
+
   const handleDateChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
     console.log(formData);
@@ -26,12 +28,28 @@ export default function Reservation() {
     setFormData((prevData) => ({ ...prevData, [name]: value }));
   };
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log("Form submitted!!", formData);
+
+    setFormData({
+      name: "",
+      email: "",
+      tel: "",
+      date: "",
+      time: "",
+      specialRequests: "",
+    });
+  };
+
   return (
     <section className="section-padding section-reservation">
       <div className="reservation-form ">
         <h2>Meeting Room Reservation at Cafe Bianco</h2>
-        <form>
-          <div className="mt-2">
+        <form onSubmit={handleSubmit}>
+          {/*name*/}
+          <div className="mt-4">
             <label htmlFor="name">Full Name</label>
             <input
               type="text"
@@ -49,8 +67,8 @@ export default function Reservation() {
               }}
             ></input>
           </div>
-
-          <div className="mt-2">
+          {/*email*/}
+          <div className="mt-3">
             <label htmlFor="email">Email Address</label>
             <input
               value={formData.email}
@@ -68,8 +86,8 @@ export default function Reservation() {
               required
             />
           </div>
-
-          <div className="mt-2">
+          {/*phone*/}
+          <div className="mt-3">
             <label htmlFor="tel">Phone Number</label>
             <input
               value={formData.tel}
@@ -87,8 +105,8 @@ export default function Reservation() {
               onChange={handleChange}
             />
           </div>
-
-          <div className="mt-2">
+          {/*reservation*/}
+          <div className="mt-3">
             <label htmlFor="date">Reservation Date</label>
             <input
               type="date"
@@ -101,8 +119,8 @@ export default function Reservation() {
               required
             />
           </div>
-
-          <div className="mt-2">
+          {/*time*/}
+          <div className="mt-3">
             <label htmlFor="time">Please pick a time.</label>
             <select
               id="time"
@@ -117,8 +135,8 @@ export default function Reservation() {
               <option value="11:00">11:00 AM</option>
             </select>
           </div>
-
-          <div className="mt-2">
+          {/*request*/}
+          <div className="mt-3">
             <label htmlFor="textarea">Special Requests?!</label>
             <textarea
               rows="101"
@@ -138,12 +156,7 @@ export default function Reservation() {
             ></textarea>
           </div>
 
-          <button
-            type="submit"
-            onSubmit={(e) => {
-              e.preventDefault();
-            }}
-          >
+          <button className="mt-3" type="submit">
             Submit !
           </button>
         </form>
